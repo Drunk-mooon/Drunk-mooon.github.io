baadbe0604065886e3f0efe1c8eafd4974efe097
yangziyue <1369700255@qq.com>
2026-07-20 14:15:07 +0800

    add picture

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://YOUR_GITHUB_USERNAME.github.io',
+  site: 'https://Drunk-mooon.github.io',
   base: '/',
   // 关闭 telemetry
   telemetry: false,
